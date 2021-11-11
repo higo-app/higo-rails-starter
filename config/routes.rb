b4479770c5746ca6c882e816147f35028b2c93ea
Santiago Perez Perret <perezperret@gmail.com>
2021-11-11 14:24:55 -0600

    Fix React route instructions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-
   # Comment out to use React
-  # root to: 'pages#index'
+  root to: 'pages#index'
   %w[about].each do |page|
-    get page, to: 'react_app#index'
+    get page, to: 'pages#show'
   end
 
   # Uncomment to use React
-  root to: 'react_app#index'
-  get '*path', to: 'react_app#index'
+  # root to: 'react_app#index'
+  # get '*path', to: 'react_app#index'
 end
